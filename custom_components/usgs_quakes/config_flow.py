9778bd63d15bf24ce85f8e0cf4dcfdcd9dec9e93
Edison Montes M. <edison.montes@gmail.com>
2025-07-12 17:02:51 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -1,11 +1,11 @@
 from homeassistant import config_entries
 import voluptuous as vol
-from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_RADIUS
 
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_RADIUS
 from .const import (
     DOMAIN,
-    VALID_FEED_TYPES,
     FEED_FRIENDLY_NAMES,
+    FRIENDLY_NAME_TO_FEED_TYPE,
     DEFAULT_RADIUS_IN_KM,
     DEFAULT_MINIMUM_MAGNITUDE,
 )
@@ -13,9 +13,6 @@ from .const import (
 CONF_FEED_TYPE = "feed_type"
 CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
 
-def _get_friendly_feed_selector():
-    """Create selector with friendly names as keys and internal ids as values."""
-    return {FEED_FRIENDLY_NAMES[feed]: feed for feed in VALID_FEED_TYPES}
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for USGS Quakes."""
@@ -24,14 +21,20 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
+            # Convert friendly name back to internal ID
+            user_input[CONF_FEED_TYPE] = FRIENDLY_NAME_TO_FEED_TYPE[user_input[CONF_FEED_TYPE]]
             return self.async_create_entry(title="USGS Quakes", data=user_input)
 
         schema = vol.Schema({
-            vol.Required(CONF_FEED_TYPE): vol.In(_get_friendly_feed_selector()),
+            vol.Required(CONF_FEED_TYPE): vol.In(list(FEED_FRIENDLY_NAMES.values())),
             vol.Required(CONF_LATITUDE, default=self.hass.config.latitude): vol.Coerce(float),
             vol.Required(CONF_LONGITUDE, default=self.hass.config.longitude): vol.Coerce(float),
             vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM): vol.Coerce(float),
             vol.Required(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): vol.Coerce(float),
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            description_placeholders={},
+        )
